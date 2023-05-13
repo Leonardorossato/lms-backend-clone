867ef89debdfc5268e471a79593583e89971d77d
Leonardorossato <leonardorossato21@gmail.com>
2023-05-13 10:07:47 -0300

    feat: create mongo.config.js connection with mongoDB

---
+++ src/config/mongo.config.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const mongoURL = process.env.MONGO_URL;
+
+const mongoConnection = () => {
+  try {
+    const connection = mongoose
+      .connect(mongoURL)
+    console.log("Connection with Mongo Databse was successfuly.")
+    return connection;
+  } catch (error) {
+    throw new Error("Could not connect to this Mongo Database", error);
+  }
+};
+
+module.exports = mongoConnection;
