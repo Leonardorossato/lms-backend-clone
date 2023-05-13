7e5e096583a9dd5910a05f8aca529816dfe174ab
Leonardorossato <leonardorossato21@gmail.com>
2023-05-13 10:07:59 -0300

    import connection with mongoDB

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoConnection = require('./config/mongo.config')
 require('dotenv').config()
 const PORT = process.env.PORT
 
 app.use(express.json())
 app.use(cors())
 
+app.listen(mongoConnection)
 app.listen(PORT, () =>{
     console.log(`Server is running at port: ${PORT}`)
 })
