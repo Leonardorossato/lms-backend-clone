adb2021163106bbf114ceaa7921c66f2e45a45ac
Leonardorossato <leonardorossato21@gmail.com>
2023-05-13 11:24:23 -0300

    update server.js import auth router

--- src/server.js
+++ src/server.js
@@ -4,11 +4,15 @@ const cors = require("cors");
 const mongoConnection = require("./config/mongo.config");
 require("dotenv").config();
 const PORT = process.env.PORT;
+const userRouter = require("./routes/user.router");
+const authRouter = require("./routes/auth.router");
 
 app.use(express.json());
 app.use(cors());
+app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(mongoConnection);
 app.listen(PORT, () => {
-  console.log(`Server is running at port: http://localhost:${PORT}/api`);
+  console.log(`Server is running at port: http://localhost:${PORT}`);
 });
