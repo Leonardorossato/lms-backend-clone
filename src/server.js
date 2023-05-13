4d524625f26f0a5da53a1c17cb61db9ccc9513ec
Leonardorossato <leonardorossato21@gmail.com>
2023-05-13 11:02:28 -0300

    update server.js

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoConnection = require('./config/mongo.config')
-require('dotenv').config()
-const PORT = process.env.PORT
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoConnection = require("./config/mongo.config");
+require("dotenv").config();
+const PORT = process.env.PORT;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.listen(mongoConnection)
-app.listen(PORT, () =>{
-    console.log(`Server is running at port: ${PORT}`)
-})
+app.listen(mongoConnection);
+app.listen(PORT, () => {
+  console.log(`Server is running at port: http://localhost:${PORT}/api`);
+});
