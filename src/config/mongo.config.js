72955c7ba013cf39bde7d6e7f19f2144a161f418
Leonardorossato <leonardorossato21@gmail.com>
2023-05-13 10:08:14 -0300

    update mongo.config.js

--- src/config/mongo.config.js
+++ src/config/mongo.config.js
@@ -4,9 +4,8 @@ const mongoURL = process.env.MONGO_URL;
 
 const mongoConnection = () => {
   try {
-    const connection = mongoose
-      .connect(mongoURL)
-    console.log("Connection with Mongo Databse was successfuly.")
+    const connection = mongoose.connect(mongoURL);
+    console.log("Connection with Mongo Databse was successfuly.");
     return connection;
   } catch (error) {
     throw new Error("Could not connect to this Mongo Database", error);
