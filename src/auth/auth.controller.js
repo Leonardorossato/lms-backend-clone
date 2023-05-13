17ac04985ce4f7befec997ce5579568f5c747b31
Leonardorossato <leonardorossato21@gmail.com>
2023-05-13 15:18:30 -0300

    update auth.controller.js login and register ready

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,7 +1,21 @@
+const generateToken = require("../middleware/jwt.middleware");
+const passwordMatchs = require("../middleware/password.match");
 const User = require("../models/users.model");
 
 class AuthController {
-  static login = async (req, res) => {};
+  static login = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email: email });
+      if (user && passwordMatchs(password)) {
+        return res
+          .status(200)
+          .json({ access_token: generateToken(user._id), role: user?.roles });
+      }
+    } catch (error) {
+      return res.status(404).json("Email or password not match", error.message);
+    }
+  };
 
   static register = async (req, res) => {
     try {
