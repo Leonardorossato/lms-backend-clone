e9940e5e6843390b980ac80d7a7015cd800f9f35
Leonardorossato <leonardorossato21@gmail.com>
2023-05-13 21:51:46 -0300

    update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+const validObjectId = require("../middleware/validate.middleware");
 const User = require("../models/users.model");
 
 class UserController {
@@ -7,6 +8,20 @@ class UserController {
       return res.status(200).json(user);
     } catch (error) {
       return res.status(500).json("Error to find all users", error.message);
+    }
+  };
+
+  static update = async (req, res) => {
+    const { _id } = req.user;
+    validObjectId(_id);
+    try {
+      const user = await User.findByIdAndUpdate(_id, req.body, { new: true });
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      return res.status(200).json({ message: "User updated successfully." });
+    } catch (error) {
+      return res.status(500).json({ message: "Error updating this user" });
     }
   };
 }
