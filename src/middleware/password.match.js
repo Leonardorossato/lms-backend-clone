ccf2765762897411bbe78a1c75430bc51f835df7
Leonardorossato <leonardorossato21@gmail.com>
2023-05-17 09:26:37 -0300

    update password.match.js

--- src/middleware/password.match.js
+++ src/middleware/password.match.js
@@ -1,9 +1,9 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/users.model");
 
-const passwordMatchs = async function(enterPassword){
-  const password = User
-  return await bcrypt.compare(password, enterPassword);
+const passwordMatchs = async function (enterPassword) {
+  const password = User;
+  return bcrypt.compare(password, enterPassword);
 };
 
 module.exports = passwordMatchs;
