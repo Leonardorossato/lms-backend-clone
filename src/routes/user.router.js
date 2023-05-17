eb5633196b4e391850b20549104df72b5914c262
Leonardorossato <leonardorossato21@gmail.com>
2023-05-17 09:26:41 -0300

    update user.router.js

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const UserController = require("../controllers/user.controller");
-const { authMiddleware } = require("../middleware/jwt.middleware");
+const { authenticationTokenUser } = require("../middleware/jwt.middleware");
 const validObjectId = require("../middleware/validate.middleware");
 const router = express.Router();
 
-router.put("/update/:id", authMiddleware, validObjectId, UserController.update);
-router.put("/block/:id", authMiddleware, validObjectId, UserController.update);
+router.put(
+  "/update/:id",
+  authenticationTokenUser,
+  validObjectId,
+  UserController.update
+);
+router.put(
+  "/block/:id",
+  authenticationTokenUser,
+  validObjectId,
+  UserController.update
+);
+router.put(
+  "/password/:id",
+  authenticationTokenUser,
+  validObjectId,
+  UserController.update
+);
 router.delete(
   "/delete/:id",
   validObjectId,
-  authMiddleware,
+  authenticationTokenUser,
   UserController.delete
 );
 
