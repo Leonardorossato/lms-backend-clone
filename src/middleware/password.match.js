2428b7cbc52e3883fdf7bca57507ce8e054621d6
Leonardorossato <leonardorossato21@gmail.com>
2023-05-17 13:34:27 -0300

    update password.match.js

--- src/middleware/password.match.js
+++ src/middleware/password.match.js
@@ -1,22 +1,9 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/users.model");
-const crypto = require("crypto");
 
 const passwordMatchs = async function (enterPassword) {
   const password = User;
   return bcrypt.compare(password, enterPassword);
 };
 
-const createPasswordResetToken = async function () {
-  const passwordResetToken = User;
-  const passwordResetExpires = User;
-  const resetToken = crypto.randomBytes(32).toString("hex");
-  passwordResetToken = crypto
-    .createHash("sha256")
-    .update(resetToken)
-    .digest("hex");
-  passwordResetExpires = Date.now() + 30 * 60 * 1000;
-  return resetToken;
-};
-
-module.exports = { passwordMatchs, createPasswordResetToken };
+module.exports = passwordMatchs;
