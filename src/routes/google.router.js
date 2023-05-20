cac0084fd6d8a5486b32491cdb6ea314aaaac46f
Leonardorossato <leonardorossato21@gmail.com>
2023-05-20 11:02:16 -0300

    update google.router.js

--- src/routes/google.router.js
+++ src/routes/google.router.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-// const { generateToken } = require("../middleware/jwt.middleware");
-// const User = require("../models/users.model");
+const { generateToken } = require("../middleware/jwt.middleware");
+const User = require("../models/users.model");
 const expressAsyncHandler = require("express-async-handler");
 
 router.get("/login/success", async (req, res) => {
-  console.log("Success!");
-  res.status(200).json({ message: "Login Sucessfully.", user: req.user });
+  if (req.user) {
+    const user = await User.findOne({ email: req.user.email });
+    if (user) {
+      return res.status(200).json({ token: generateToken(user._id), from: "google" });
+    } else {
+      return res.status(401).json({ message: "Error in generate this token" });
+    }
+  }
+  return res.status(404).json({ error: "Error login with this email address" });
 });
 
 router.get("/login/failed", async (req, res) => {
