ca897349f043f72578871a36bfb28bd9efd4e44f
Leonardorossato <leonardorossato21@gmail.com>
2023-05-22 13:29:39 -0300

    update review.model.js

--- src/models/review.model.js
+++ src/models/review.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const reviewSchema = new mongoose.Schema(
   {
-    userId: {
+    user: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
       ref: "User",
