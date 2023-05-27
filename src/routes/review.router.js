d273dcc61499bd2bef5072155509131fc0fcf498
Leonardorossato <leonardorossato21@gmail.com>
2023-05-27 13:40:25 -0300

    update review.router.js

--- src/routes/review.router.js
+++ src/routes/review.router.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const ReviewController = require("../controllers/review.controller");
-const { authMiddlewareToken } = require("../middleware/jwt.middleware");
+const {
+  authMiddlewareToken,
+  isAdminToken,
+} = require("../middleware/jwt.middleware");
 const router = express.Router();
 
 router.post("/create", authMiddlewareToken, ReviewController.create);
+router.get("/all", ReviewController.findAll);
+router.get("/:id", authMiddlewareToken, isAdminToken, ReviewController.findOne);
+router.put("/:id", authMiddlewareToken, isAdminToken, ReviewController.update);
+router.delete(
+  "/:id",
+  authMiddlewareToken,
+  isAdminToken,
+  ReviewController.delete
+);
 
 module.exports = router;
