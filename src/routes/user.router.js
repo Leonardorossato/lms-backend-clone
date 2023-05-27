7ba7d999bce251dd9adbdf72f02d62cfb920af2c
Leonardorossato <leonardorossato21@gmail.com>
2023-05-27 10:56:34 -0300

    update user.router.js

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const UserController = require("../controllers/user.controller");
-const { authmiddlewareToken } = require("../middleware/jwt.middleware");
 const validObjectId = require("../middleware/validate.middleware");
+const { authMiddlewareToken } = require("../middleware/jwt.middleware");
 const router = express.Router();
 
 router.post("/forgot-password", UserController.forgotPassword);
 
 router.put(
   "/update/:id",
-  authmiddlewareToken,
+  authMiddlewareToken,
   validObjectId,
   UserController.update
 );
@@ -17,14 +17,14 @@ router.put("/reset-password/:token", UserController.resetPassword);
 
 router.put(
   "/update-password/:id",
-  authmiddlewareToken,
+  authMiddlewareToken,
   validObjectId,
   UserController.updatePassword
 );
 router.delete(
   "/delete/:id",
   validObjectId,
-  authmiddlewareToken,
+  authMiddlewareToken,
   UserController.delete
 );
 
